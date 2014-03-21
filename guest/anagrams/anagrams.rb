07257d6e8c164d6e350e28ca6178236471658f5e
am2100 <jimnoble@xjjz.co.uk>
2014-03-21 16:57:07 +0000

    Start anagrams

---
+++ guest/anagrams/anagrams.rb
@@ -0,0 +1,26 @@
+# Author: Jim Noble
+# jimnoble@xjjz.co.uk
+#
+# anagrams.rb
+#
+# A program that takes a list of words and outputs an array of anagram arrays
+#
+
+class Anagramalam
+  def initialize(words)
+    @input = words
+    @output = Hash.new
+  end
+
+  def combine_anagrams(@input)
+    # read in each line of the file
+    
+    # sort line alphabetically
+
+    # check to see if the sorted line already exists as a hash name
+
+    # if it doesn't, create it, and assign a new array as it's value. Add word to value array.
+
+    # if it does, add word to existing value array
+  end
+end
